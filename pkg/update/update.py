65cbf29e4a72b14658587a149c03b87c27df659f
AzureTecDevs <150609052+AzureTecDevs@users.noreply.github.com>
2023-11-26 23:25:35 -0500

    Update update.py

--- pkg/update/update.py
+++ pkg/update/update.py
@@ -3,4 +3,4 @@ import requests
 print('Updating...')
 url = 'https://raw.githubusercontent.com/AzureTecDevs/pyOS/main/init.py'
 r = requests.get(url, allow_redirects=True)
-open(f'init.py', 'wb').write(r.content)
+open(f'../../init.py', 'wb').write(r.content)
