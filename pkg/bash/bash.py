49d2a2dd5c0cf00f7f4788af06bbfb82c2cf3980
AzureTecDevs <150609052+AzureTecDevs@users.noreply.github.com>
2023-11-27 00:00:34 -0500

    Delete pkg directory

--- pkg/bash/bash.py
+++
@@ -1,9 +0,0 @@
-import os
-print("Type 'EXIT' to exit BASH")
-pr = "$"
-while True:
-    o = input(f"BASH {pr} ")
-    if not o.lower() == "exit":
-        os.system(o)
-    else:
-        break
--- pkg/update/update.py
+++
@@ -1,6 +0,0 @@
-import os
-import requests
-print('Updating...')
-url = 'https://raw.githubusercontent.com/AzureTecDevs/pyOS/main/init.py'
-r = requests.get(url, allow_redirects=True)
-open(f'../init.py', 'wb').write(r.content)
