c3657e8faaedf6502fc1b28028f5942cae5c0171
AzureTecDevs <150609052+AzureTecDevs@users.noreply.github.com>
2023-11-27 22:41:25 -0500

    bugfix

--- pkg/gui/gui.py
+++ pkg/gui/gui.py
@@ -1,6 +1,6 @@
 import os
 i = input("GUI Mode: tkinter, qt")
 if i.lower() == "qt":
-    os.system("python3 qt.py")
+    os.system("python3 pkg/gui/qt.py")
 else:
-    os.system("python3 tk.py")
+    os.system("python3 pkg/gui/tk.py")
